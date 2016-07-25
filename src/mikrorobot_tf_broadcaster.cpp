da7544c5cc33cf6cc2b9d6dd431bbd6d0319030a
bjornite <bjornite@student.matnat.uio.no>
2016-07-25 22:32:49 +0200

    Laserscan, odometry, IMU and mapping now works with Gazebo, also added launchfile

--- src/mikrorobot_tf_broadcaster.cpp
+++ src/mikrorobot_tf_broadcaster.cpp
@@ -9,35 +9,14 @@ int main(int argc, char** argv){
     ros::Rate r(100);
 
     tf::TransformBroadcaster broadcaster;
-    tf::TransformListener listener;
 
     while(n.ok()){
 
-        tf::StampedTransform world_to_camera_transform;
-        tf::StampedTransform camera_to_base_transform;
-        try{
-            listener.lookupTransform("ORB_SLAM/World", "ORB_SLAM/Camera",
-                                     ros::Time(0), world_to_camera_transform);
-            listener.lookupTransform("camera", "base_link",
-                                     ros::Time(0), camera_to_base_transform);
-        }
-        catch (tf::TransformException ex){
-            ROS_ERROR("%s",ex.what());
-            ros::Duration(1.0).sleep();
-        }
+        broadcaster.sendTransform(tf::StampedTransform(tf::Transform().getIdentity(), ros::Time::now(), "base_footprint", "base_link"));
+         broadcaster.sendTransform(tf::StampedTransform(tf::Transform().getIdentity(), ros::Time::now(), "world", "map"));
+          broadcaster.sendTransform(tf::StampedTransform(tf::Transform().getIdentity(), ros::Time::now(), "odom", "odom_combined"));
 
-        broadcaster.sendTransform(
-            tf::StampedTransform(
-                tf::Transform().getIdentity(),
-                ros::Time::now(),"/ORB_SLAM/World", "map"));
-        broadcaster.sendTransform(
-            tf::StampedTransform(
-                world_to_camera_transform,
-                ros::Time::now(),"map", "odom"));
-        broadcaster.sendTransform(
-            tf::StampedTransform(
-                camera_to_base_transform,
-                ros::Time::now(),"odom", "base_link"));
+
         r.sleep();
     }
 }
